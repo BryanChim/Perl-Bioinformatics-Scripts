00a96d2137edcb4cdaf56499c7638ec4263f7e59
BryanChim <bryanchim88@gmail.com>
2014-11-12 23:46:02 -0500

    added further documentation in header

--- BlastParser.pl
+++ BlastParser.pl
@@ -4,9 +4,15 @@
 #### BLASTPARSER
 
 #    ORIGINAL AUTHORS: Jeff Elhai and Paul Fawcett (Fall 2004)
-#	 MODIFICATIONS BY: Bryan Chim (Fall 2013)
+#
+#	 MODIFICATIONS BY: Bryan Chim (Fall 2013) 
+#		** Update regexes to account for change in BLASTn output format
+#		** Additional output file for queries that made no hits against the target
+#		** Account for query and contig description "spillover" across lines
+#
 #    PURPOSE: Extract information from BLAST output
 #	 		  Compatible with output from BLASTn 2.2.28+
+#
 #    INPUT: From file $blast_path, with format:
 #       (header)
 #       ...
@@ -31,6 +37,10 @@
 #    OUTPUT: From file $result_path, with format:
 #       query_name query_description query_length subject1_name
 #              subject1_description subject1_length
+#
+#****OUTPUT 2: From file $result_path_nohits:
+#		- Same format as $result_path output -- restricted to information for
+#			queries that yielded no hits against target
 #
 #       NOTE: If there is more than one matching subject, then
 #             subject2_name subject2_description subject2_length is appended
@@ -65,7 +75,7 @@
   my $blast_path = "C:/Users/Public/O157K12COMP";
   open BLAST, "<$blast_path" or die "Can't open $blast_path: $!\n";
 
-#### OPEN OUR OUTPUT FILE
+#### OPEN OUR OUTPUT FILES
 
   my $result_path = "C:/Users/Public/O157K12COMPparse";
   open RESULT, ">$result_path" or die "Can't open $result_path: $!\n";
@@ -88,34 +98,29 @@
 # The other possibility is that we might see the start of a match.  If so,
 # we record it in @query_info.
 
-  $line = <BLAST>;                      # Read first line
-  while (defined $line) {
+$line = <BLAST>;                      
+while (defined $line) 
+{
 
-  if ($line =~ /^[*****]/)
-        {print_previous_query_nohits();
-       }
+	if ($line =~ /^[*****]/)	# In BLASTn 2.2.28, "No-Hit" queries begin with "***** NO HITS ..."***
+	{
+		print_previous_query_nohits();  
+	}
 
-                    # Only if not end of file
-     if ($line =~ /^Query=/) {          # If 'Query' matches beginning of line...
-
-
+                    
+	if ($line =~ /^Query=/) 	# If 'Query' matches beginning of line, print previous query data
+	{          
         print_previous_query();
-
-
-#     if ($line =~ /\s*No hits found/)
-#        {print_previous_query_nohits();
-#       }
-
-
         start_new_query();
+    }
 
-     }
-
-
-     if ($line =~ /^>Contig/) { record_subject() }
-                                        # If '>Contig' matches beginning of line...
-     $line = <BLAST>;                   # Read next line
-  }
+    if ($line =~ /^>Contig/) 	# If '>Contig' matches beginning of line, get subject data
+	{ 
+	record_subject() 	 
+	}
+                                       
+    $line = <BLAST>;                   # Read next line
+}
 
 # Now for the last query.  All the others were printed when we were
 # about to process the query after them; but there is no query after the
@@ -125,14 +130,14 @@
 
 #################### SUBROUTINES ####################
 
-#### PRINT_PREVIOUS_QUERY
+#### PRINT_PREVIOUS_QUERY and PRINT_PREVIOUS_QUERY_NOHITS
 # Prints info stored in @query_info
 # If there's no info there (i.e., the first time through), print nothing
 
-sub print_previous_query {
-
-
-   if (@query_info) {
+sub print_previous_query 
+{
+   if (@query_info) 
+   {
       my $result_line= join(",", @query_info); # Put comma between data
       print $result_line, "\n";                 # Print to monitor
       print RESULT $result_line, "\n";          # Print to file
@@ -140,13 +145,12 @@ sub print_previous_query {
    }
 }
 
-
-
-sub print_previous_query_nohits {
-
-   if (@query_info) {
-     my $result_line= join(",", @query_info); # Put comma between data
-      print RESULTS_NOHITS $result_line, "\n";          # Print to file
+sub print_previous_query_nohits 
+{
+   if (@query_info) 
+   {
+		my $result_line= join(",", @query_info); # Put comma between data
+		print RESULTS_NOHITS $result_line, "\n";          # Print to file
                              # Reinitialize storage to nothing
    }
 }
@@ -154,54 +158,67 @@ sub print_previous_query_nohits {
 #### START_NEW_QUERY
 # Extracts query information, saves in @query_info
 
-sub start_new_query {
+sub start_new_query 
+{
 
    ($query_name, $query_description) = ($line =~ /^Query=\s(\w+)\W+(.+)/);
 
    $line = <BLAST>;
     chomp $line;
-    if (defined ($line))
+    
+	if (defined ($line))
     {
-
     $query_description = ($query_description . " " . $line);
     }
     
-   unless (defined $query_name) {
+	unless (defined $query_name) 
+    {
       warn "Didn't find query name on line $.\n";
       return;
-   }
+    }
 
    $line = <BLAST>;
-   while (defined $line) {
+   
+	while (defined $line) 
+	{
 
-      ($query_length) = ($line =~ /Length=(\d+)/);
-      if (defined $query_length) {
-         @query_info = ($query_name, $query_description, $query_length);
-         return;
-      }
+		($query_length) = ($line =~ /Length=(\d+)/);
+		
+		if (defined $query_length) 
+		{
+			@query_info = ($query_name, $query_description, $query_length);
+			return;
+		}
+		
       $line = <BLAST>;
-   }
-
+	  
+	}
+	
+}
 
 #### RECORD_SUBJECT
 # Extracts subject info, stores in @query_info
 
-sub record_subject {
+sub record_subject 
+{
 
    find_subject_name();
-   unless (defined $subject_name) {
+   unless (defined $subject_name) 
+   {
       warn "Didn't find subject_name on line $.\n";
       return;
    }
 
    find_subject_length();
-   unless (defined $subject_length) {
+   unless (defined $subject_length) 
+   {
       warn "Didn't find subject_length on line $.\n";
       return;
    }
 
    find_expectation();
-   unless (defined $expectation) {
+   unless (defined $expectation) 
+   {
       warn "Didn't find expectation of chance match on line $.\n";
       return;
    }
@@ -214,10 +231,11 @@ sub record_subject {
 #### FIND_SUBJECT_NAME
 # Extracts name of subject
 
-sub find_subject_name {
-
+sub find_subject_name 
+{
    $subject_name = undef;
-   if ($line =~ /^>Contig(\d+)\.revised\.gene(\d+)\.protein\W+(.*)/) {
+   if ($line =~ /^>Contig(\d+)\.revised\.gene(\d+)\.protein\W+(.*)/) 
+   {
      $subject_name = sprintf "%03d.%03d", $1, $2;
      $subject_description = $3;
    }
@@ -226,15 +244,18 @@ sub find_subject_name {
 #### FIND_SUBJECT_LENGTH
 # Extracts length of subject
 
-sub find_subject_length {
-
+sub find_subject_length 
+{
    $subject_length = undef;
    $line = <BLAST>;
-   while (defined $line) {
+   
+   while (defined $line) 
+   {
       ($subject_length) = ($line =~ /Length= (\d+)/);
-      if (defined $subject_length) {
-         return;
-      }
+	  
+      if (defined $subject_length) 
+	  {return;}
+	  
       $line = <BLAST>;
    }
 }
@@ -242,16 +263,22 @@ sub find_subject_length {
 #### FIND_EXPECTATION
 # Extracts E-value for comparison
 
-sub find_expectation {
+sub find_expectation 
+{
 
    $expectation = undef;
    $line = <BLAST>;
+   
    if (defined $line) { $line = <BLAST> }
-   unless (defined $line) {
+   
+   unless (defined $line) 
+   {
       warn "Incomplete query at $.\n";
       return;
    }
+   
    ($expectation) = ($line =~ /Expect = (\S+)/);
+
 }
-}
+
 
